2e43014ade7ee1eb5689a68261509aac05d4b79c
Björn Olsson Ågren <bjorn.olssonagren@inera.se>
2025-11-25 14:22:56 +0100

    NTU-662: Open up reflection access.

--- docker_context/setenv.sh
+++ docker_context/setenv.sh
@@ -3,6 +3,8 @@ export CLASSPATH=$CATALINA_HOME/log4j2/lib/*:$CATALINA_HOME/log4j2/conf
 CATALINA_OPTS+=" -Dlog4j.configurationFile=\"file://${CATALINA_HOME}/conf/log4j2.xml\""
 CATALINA_OPTS+=" -Dlog4j.configuration=\"file://${CATALINA_HOME}/conf/log4j.xml\""
 CATALINA_OPTS+=" -XX:MaxRAMPercentage=75.0"
+# Java 17+ tightens module access; allow CXF to set Authenticator via reflection
+CATALINA_OPTS+=" --add-opens=java.base/java.net=ALL-UNNAMED"
 
 if [ -n "$AXEL_HOME" ]; then
   CATALINA_OPTS+=" -Daxel.home=\"$AXEL_HOME\""
